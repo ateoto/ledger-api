853131a5edf1a2f0301c1d3520353a2bd4753b02
ateoto <mattmccants@gmail.com>
2020-03-13 15:07:32 -0400

    First pass on JWT with cognito

--- ledger/api/api_v1/api.py
+++ ledger/api/api_v1/api.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from ledger.schemas.JWT import JWTBearer
+from ledger.api.utils.auth import jwks
 from ledger.api.api_v1.endpoints import categories, users
+
+
+auth = JWTBearer(jwks)
 
 api_router = APIRouter()
 api_router.include_router(categories.router, prefix="/categories", tags=["categories"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(users.router, prefix="/users", dependencies=[Depends(auth)], tags=["users"])
---
+++ ledger/api/utils/auth.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from fastapi import Depends, HTTPException
+from starlette.status import HTTP_403_FORBIDDEN
+
+
+from ledger.schemas.JWT import JWKS, JWTBearer, JWTAuthorizationCredentials
+
+jwks = JWKS.parse_obj(
+    requests.get(
+        f"https://cognito-idp.{os.environ.get('COGNITO_REGION')}.amazonaws.com/"
+        f"{os.environ.get('COGNITO_POOL_ID')}/.well-known/jwks.json"
+    ).json()
+)
+
+auth = JWTBearer(jwks)
+
+
+async def get_current_user(
+        credentials: JWTAuthorizationCredentials = Depends(auth)
+) -> str:
+    try:
+        return credentials.claims["username"]
+    except KeyError:
+        HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Username missing")
